0866803b2ff80e1c1f7f04426c5483b3355fea46
lyipei <lin23027@byui.edu>
2024-04-27 12:50:42 -0600

    p2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int number = int.Parse(answer);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep working hard for next time!");
+        }
     }
 }
