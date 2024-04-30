15b5831888d6b835c1e3b8fb92f093efcbd9074a
lyipei <lin23027@byui.edu>
2024-04-30 17:47:31 -0600

    P3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random numberGenerator = new Random();
+        int magic = numberGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        for (int x=0; x<9 ; x++ )
+        {
+            Console.WriteLine("The most interesting thing!");
+        }
+    
     }
 }
